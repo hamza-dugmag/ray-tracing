649c944e297412037297caca900f5ee727a25ce6
Hamza Dugmag <hdugmag@gmail.com>
2022-07-02 20:43:54 -0400

    Render flatly lit sphere.

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -24,12 +24,43 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 
 void Renderer::Render()
 {
-	// Randomize image
-	for (uint32_t i = 0; i < m_FinalImage->GetWidth() * m_FinalImage->GetHeight(); i++)
+	// y then x is more efficient
+	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
 	{
-		m_ImageData[i] = Walnut::Random::UInt();  // 0xABGR
-		m_ImageData[i] |= 0xff000000;
+		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
+		{
+			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth() , (float)y / (float)m_FinalImage->GetHeight() };
+			coord = coord * 2.0f - 1.0f;  // -1 -> 1
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+		}
 	}
 
 	m_FinalImage->SetData(m_ImageData);  // Render image
+}
+
+uint32_t Renderer::PerPixel(glm::vec2 coord)
+{
+	uint8_t r = (uint8_t)(coord.x * 255.0f);
+	uint8_t g = (uint8_t)(coord.y * 255.0f);
+
+	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+	// rayDirection = glm::normalize(rayDirection);
+	float radius = 0.5f;
+
+	// 2D: (bx^2 + by^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0
+	// a = ray origin
+	// b = ray direction
+	// r = circle radius
+	// t = hit distance
+
+	float a = glm::dot(rayDirection, rayDirection);
+	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
+	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+
+	float discriminant = b * b - 4.0f * a * c;
+	if (discriminant >= 0.0f)
+		return 0xffff00ff;
+
+	return 0xff000000;
 }
